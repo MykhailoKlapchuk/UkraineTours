78294326799939e10f620b5bb8085ab69cca8470
Mykhailo Klapchuk <klapchuk.m@gmail.com>
2021-12-22 19:24:03 +0200

    Added login/register ui logic

--- UkraineToursClient/src/app/app.module.ts
+++ UkraineToursClient/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 import { TourCardComponent } from './tour/tour-card/tour-card.component';
@@ -11,12 +13,18 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { TouringService } from './services/touring.service';
 import { AddTourComponent } from './tour/add-tour/add-tour.component';
 import { TourDetailComponent } from './tour/tour-detail/tour-detail.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserServiceService } from './services/user-service.service';
+import { AlertifyService } from './services/alertify.service';
 
 const appRoutes: Routes = [
   {path: '', component: TourListComponent},
   {path: 'group-tour', component: TourListComponent},
   {path: 'add-tour', component: AddTourComponent},
   {path: 'tour-detail/:id', component: TourDetailComponent},
+  {path: 'user/login', component: UserLoginComponent},
+  {path: 'user/register', component: UserRegisterComponent},
   {path: '**', component: TourListComponent}
 ]
 
@@ -27,16 +35,23 @@ const appRoutes: Routes = [
     TourListComponent,
     NavBarComponent,
     AddTourComponent,
-    TourDetailComponent
+    TourDetailComponent,
+    UserRegisterComponent,
+    UserLoginComponent
    ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [
-    TouringService
+    TouringService,
+    UserServiceService,
+    AlertifyService
   ],
   bootstrap: [AppComponent]
 })
---
+++ UkraineToursClient/src/app/user/user-register/user-register.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { UserForRegister } from 'src/app/model/user';
+import { AlertifyService } from 'src/app/services/alertify.service';
+//import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+    selector: 'app-user-register',
+    templateUrl: './user-register.component.html',
+    styleUrls: ['./user-register.component.css']
+})
+export class UserRegisterComponent implements OnInit {
+
+    registrationForm: FormGroup;
+    user: UserForRegister;
+    userSubmitted: boolean;
+    constructor(private fb: FormBuilder,
+                //private authService: AuthService,
+                private alertify: AlertifyService
+                ) { }
+
+    ngOnInit() {
+        this.createregistrationForm();
+    }
+
+    createregistrationForm() {
+        this.registrationForm =  this.fb.group({
+            userName: [null, Validators.required],
+            email: [null, [Validators.required, Validators.email]],
+            password: [null, [Validators.required, Validators.minLength(8)]],
+            confirmPassword: [null, Validators.required],
+            mobile: [null, [Validators.required, Validators.maxLength(10)]]
+        }, {validators: this.passwordMatchingValidatior});
+    }
+
+    passwordMatchingValidatior(fg: FormGroup): Validators {
+        return fg.get('password').value === fg.get('confirmPassword').value ? null :
+            {notmatched: true};
+    }
+
+
+    onSubmit() {
+        console.log(this.registrationForm.value);
+        this.userSubmitted = true;
+
+        if (this.registrationForm.valid) {
+            //this.authService.registerUser(this.userData()).subscribe(() =>
+            //{
+                this.onReset();
+                this.alertify.success('Congrats, you are successfully registered');
+           // });
+        }
+    }
+
+    onReset() {
+        this.userSubmitted = false;
+        this.registrationForm.reset();
+    }
+
+    userData(): UserForRegister {
+      return this.user = {
+          userName: this.userName.value,
+          email: this.email.value,
+          password: this.password.value,
+          mobile: this.mobile.value
+      };
+  }
+
+
+    // ------------------------------------
+    // Getter methods for all form controls
+    // ------------------------------------
+    get userName() {
+        return this.registrationForm.get('userName') as FormControl;
+    }
+
+    get email() {
+        return this.registrationForm.get('email') as FormControl;
+    }
+    get password() {
+        return this.registrationForm.get('password') as FormControl;
+    }
+    get confirmPassword() {
+        return this.registrationForm.get('confirmPassword') as FormControl;
+    }
+    get mobile() {
+        return this.registrationForm.get('mobile') as FormControl;
+    }
+}
